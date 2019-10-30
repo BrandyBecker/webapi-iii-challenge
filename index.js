fc5244accf32afd1d3cb3816a9931fbaee0b8248
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-30 16:55:17 -0400

    + Added a Custom Logger to log the HTTP Request, the URL, the date + time it was logged.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server.js');
+
+const port = 3030;
+server.listen(port, () => {
+  console.log(`\n===  ⭐  API on Port ${port} ⭐   ===\n`);
+});
+
+module.exports = port;
