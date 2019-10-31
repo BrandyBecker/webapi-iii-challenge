fdc895ce7eb1fbcc94b74ea50ffa440aebbdd047
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-31 16:58:01 -0400

    + updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server.js');
 
-const port = 3030;
+const port = process.env.PORT || 3030;
 server.listen(port, () => {
   console.log(`\n===  ⭐  API on Port ${port} ⭐   ===\n`);
 });
